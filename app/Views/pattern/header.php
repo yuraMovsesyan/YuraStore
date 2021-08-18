b13fcea892c0552553eaedf9abf23da1919e862e
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2021-08-18 18:53:49 +0300

    add modal window

--- app/Views/pattern/header.php
+++ app/Views/pattern/header.php
@@ -271,7 +271,19 @@
                                                                 </ul>
                                                         </div>
                                                         <?php else: ?>
-                                                            <a class="uk-navbar-item uk-link-muted tm-navbar-button" href="/login" uk-icon="sign-in"></a>
+                                                        <a class="uk-navbar-item uk-link-muted tm-navbar-button" href="#modal-example" uk-toggle uk-icon="sign-in"></a>
+
+                                                        <div id="modal-example" uk-modal>
+                                                                <div class="uk-modal-dialog uk-modal-body">
+                                                                        <h2 class="uk-modal-title">Headline</h2>
+                                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                                        <p class="uk-text-right">
+                                                                        <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+                                                                        <button class="uk-button uk-button-primary" type="button">Save</button>
+                                                                        </p>
+                                                                </div>
+                                                        </div>
+
                                                         <?php endif ?>
                                                  </div>
                                         </div>
